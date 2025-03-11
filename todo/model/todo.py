23a11c70d474fb5587f3be707c26109073c2fb68
Juan Esteban Echavarria <juanestebanechavarria511@gmail.com>
2025-03-11 13:36:20 -0500

    punto 2 a 6

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -10,5 +10,30 @@ class Todo:
 
     def mark_completed(self):
         self.completed: bool = True
-        pass
+
+    def add_tag(self, tags: list[str]):
+        self.tags: list[str] = tags
+
+    def __str__(self):
+        return f"{self.code_id} - {self.title}"
+
+class TodoBook:
+    def __init__(self):
+        self.todos: dict[int, Todo] = {}
+
+
+    def add_todo(self, title: str, description: str) ->int:
+
+        new_id = len(self.todos) + 1
+        new_todo = Todo(new_id, title, description)
+        self.todos[new_id] = new_todo
+        return new_id
+
+    def pending_todos(self):
+
+
+
+
+
+
 
